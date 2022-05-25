31b45a46e3ae2bfdec8344bfdbcc1b6da7f98331
aaidanerai00 <aidanerai34@gmail.com>
2022-05-25 10:29:09 +0600

    descriptionoflaptop.py

---
+++ descriptionoflaptop.py
@@ -0,0 +1,8 @@
+machine = 'hp 14-dk0xxx chassis type:10serial'
+system = 'kernel:5.4.0-58 - generic x86bit'
+desktop = 'Cinnamon 4.8.5 Linux Mint 20.1 Ulyssa base:Ubuntu 20.04'
+baterry = 'Hewlett-Packard Primary 11.8/11.6'
+CPU = 'Dual Core model: AMD A6-9225 RADEON R4 5COMPUTE CORES '
+graphics = 'AMD stoney DRM 3.35.0 5.4.0 -58-generic LLVM 10.0.0 '
+display = '1920x1080~60Hz'
+drives = 'Local Storage : total 119.24 GiB ' 
